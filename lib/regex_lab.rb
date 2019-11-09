3f990f9f75f6beb67e131b6e3b84fa1034631960
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-09 17:47:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,9 +23,11 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\d{10}/) && phone.match(/\w*/) == nil
-    p 
-    return true 
+  if phone.match(/\d{3}+\s?+\d{3}+\s?+\d{4}/) 
+    p "digits"
+    if phone.match(/\w*/) == nil  
+      p "no words"
+      return true 
   else
     return false
   end
