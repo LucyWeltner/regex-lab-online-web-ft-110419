b1d573c1e492487cc4899ceb14737e233583c128
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-09 17:44:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -36,5 +36,5 @@ p words_five_letters_long("Now there are words with five letters total")
 p starts_with_a_vowel?("cakes")
 phone_array = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
 phone_array.each do |number|
-  p valid_phone_number(number)
+  p valid_phone_number?(number)
 end
